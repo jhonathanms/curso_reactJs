30e82150f58861473915296c4a83361914d815f6
Jhonathan Martins <jhonathan.dsn.shop@alterdata.com.br>
2022-08-24 23:46:14 -0300

    criado projeto de estudo do TS

---
+++ 11_TS_INTRO/index.js
@@ -0,0 +1,9 @@
+var nome = "Jhonathan";
+var surname = "Martins";
+var idade = 27;
+var viewUser = function (value) {
+    console.log("Valor a ser impresso: ".concat(value));
+};
+viewUser(nome);
+viewUser(surname);
+viewUser(idade);
---
+++ 11_TS_INTRO/index.ts
@@ -0,0 +1,11 @@
+const nome = "Jhonathan";
+const surname = true
+const idade = 27;
+
+const viewUser = (value: string) => {
+    console.log( `Valor a ser impresso: ${value}`)
+}
+
+viewUser(nome);
+// viewUser(surname);
+// viewUser(idade);
