4ba154cff0373dc5a0032889d4662d3047566620
Jhonathan Martins <jhonathan.dsn.shop@alterdata.com.br>
2022-09-18 14:34:55 -0300

    atualização de projeto

--- 12_TS COM REACT/aula_ts_react/aula_ts_react/src/App.tsx
+++ 12_TS COM REACT/aula_ts_react/aula_ts_react/src/App.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { type } from "@testing-library/user-event/dist/type";
+import { createContext, useState } from "react";
 import DestructuringComponent from "./components/DestructuringComponent";
 import EnumComponents, {CategoriasEnum} from "./components/EnumComponents";
 import TesteComponent from "./components/PrimeiroComponent";
@@ -11,13 +12,30 @@ interface IAppContext {
   language: string;
   framework: string;
   projects: number;
+  aluno: Pessoa | null;
+  upAluno: React.Dispatch<React.SetStateAction<Pessoa | null>>
+}
+
+interface Pessoa {
+  id: string;
+  nome: string;
+  idade: number;
 }
 
 // 3- Passo: Criar o createContext. O contexto precisa ser exportado
 export const AppContext = createContext<IAppContext | null> (null);
 
-
 function App() {
+
+  const [aluno, setAluno] = useState<Pessoa | null>(null);
+  // 1- Passo
+    const contextValue:IAppContext = {
+      language: "JavaScript",
+      framework: "EXpress",
+      projects: 5,
+      aluno: aluno,
+      upAluno: setAluno
+    }
 
 // Variaveis:
   const nome: string = "Jhonathan";
@@ -28,12 +46,6 @@ function App() {
     return "Olá " + nome + ".";
   }
 
-// 1- Passo
-  const contextValue:IAppContext = {
-    language: "JavaScript",
-    framework: "EXpress",
-    projects: 5,
-  }
 
   return (
     // 4- Passo
@@ -51,7 +63,7 @@ function App() {
       <SegunComponent name="Props_Jhonathan" />
       <DestructuringComponent codigo={123456} descricao="PRODUTO 01" status={true} observacoes={["GELO", "LIMÃO", "SEM AÇUCAR"]} />
       <UseStateComponent />
-      <EnumComponents categorias={CategoriasEnum.JS} />
+      <EnumComponents categorias={CategoriasEnum.TS} />
       <Context />
         
       </div>
--- 12_TS COM REACT/aula_ts_react/aula_ts_react/src/Context/Context.tsx
+++ 12_TS COM REACT/aula_ts_react/aula_ts_react/src/Context/Context.tsx
@@ -1,24 +1,41 @@
-import React, { useContext } from 'react';
-import {AppContext} from '../App'
+import React, { useContext, useState, ChangeEvent } from 'react';
+import { AppContext } from '../App'
 
+// Não precisará desta interface pq não usarei as props.
 interface IAppProps {
 }
 
-export default function App(props: IAppProps) {
-    
-    const detalhes = useContext(AppContext);
+export default function App() {
 
+  const detalhes = useContext(AppContext);
+  const [id, setId] = useState<string | null>(null);
+  const [nome, setNome] = useState<string | null>(null);
+  const [idade, setIdade] = useState<number | null>(null);
 
   return (
     <>
-          {detalhes && (
-              <div>
-                  <hr />
-                  <h1>Contex api</h1>
-                  <h2>Linguagem: { detalhes.language }</h2>
-                  <h3>Framework: { detalhes.framework }</h3>
-                  <h4>Projects: { detalhes.projects }</h4>
-              </div>
+      {detalhes && (
+        <div>
+          <hr />
+          <h1>Contex api</h1>
+          <h2>Linguagem: {detalhes.language}</h2>
+          <h3>Framework: {detalhes.framework}</h3>
+          <h4>Projects: {detalhes.projects}</h4>
+          <div>
+            <span>Detalhes do aluno: </span>
+            <ul>
+              <li><h4>Id: {detalhes.aluno?.id}</h4>
+                <input type="text" value={id?id:""} onChange={(e: ChangeEvent<HTMLInputElement>) => setId(e.target.value)} />
+              </li>
+              <li><h4>Nome: {detalhes.aluno?.nome}</h4>
+              <input type="text" value={nome?nome:""} onChange={(e: ChangeEvent<HTMLInputElement>) => setNome(e.target.value)} />
+              </li>
+              <li><h4>Idade: {detalhes.aluno?.idade}</h4>
+              <input type="text" value={idade?idade:1} onChange={(e:ChangeEvent<HTMLInputElement>) => setIdade(parseInt(e.target.value))} />
+              </li>
+            </ul>
+          </div>
+        </div>
       )}
     </>
   );
